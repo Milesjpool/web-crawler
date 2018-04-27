64f872d399efe647ab6e953fef0ba015b1fa3a53
Miles Pool <MilesJPool@gmail.com>
2018-04-27 03:09:14 +0100

    Make relative references more intelligent

--- internal/webcrawler/subpageproviders/absolutesubpageprovider.go
+++ internal/webcrawler/subpageproviders/absolutesubpageprovider.go
@@ -10,19 +10,13 @@ func (s AbsoluteSubPageProvider) GetSubPages(pageUrl *url.URL) []url.URL {
 	subPages := s.SubPageProvider.GetSubPages(pageUrl)
 
 	for i, subPageUrl := range subPages {
+		if !subPageUrl.IsAbs(){
+			subPages[i] = *pageUrl.ResolveReference(&subPageUrl)
+		}
 		subPages[i].Fragment = ""
 		subPages[i].RawQuery = ""
-		if !subPageUrl.IsAbs(){
-			makeAbsolute(pageUrl, &subPages[i])
-		}
 	}
 
 	return subPages
-}
-
-func makeAbsolute(pageUrl *url.URL, subPageUrl *url.URL) {
-
-	subPageUrl.Host = pageUrl.Host
-	subPageUrl.Scheme = pageUrl.Scheme
 }
 
